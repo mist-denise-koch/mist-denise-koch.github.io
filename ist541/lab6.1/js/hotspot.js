f39646ac04d7349b125fd7d326232651afd9cee7
mist-denise-koch <123275394+mist-denise-koch@users.noreply.github.com>
2023-10-12 18:40:11 -0700

    eee

--- ist541/lab6.1/js/hotspot.js
+++ ist541/lab6.1/js/hotspot.js
@@ -8,7 +8,7 @@ $('#myModal').modal("show");
 
 if ($(this).attr("id") == "Gmail") {
 $("#feedback").css("color","red");
-$("#feedback").html("Although you can access Google Chat and Spaces, there is a faster way to meet in real time");
+$("#feedback").html("Although you can access Google Chat and Spaces in Gmail, there is an easier way to meet in real time");
 }
 			
 if ($(this).attr("id") == "Meet") {
@@ -16,14 +16,19 @@ $("#feedback").css("color","green");
 $("#feedback").html("Yes, accessing Google Meet allows you to meet virtually in real time to collaborate");
 }
 			
-if ($(this).attr("id") == "Drive") {
+if ($(this).attr("id") == "Calendar") {
 $("#feedback").css("color","red");
-$("#feedback").html("No,but this is a great place to schedule a virtual meeting");
+$("#feedback").html("No,but Google Calendar is a great place to schedule a virtual meeting");
 }
 			
 if ($(this).attr("id") == "SharedDocs") {
 $("#feedback").css("color","red");
 $("#feedback").html("No,but this is a great place to collaborate on group documents");
+}
+	
+if ($(this).attr("id") == "Drive") {
+$("#feedback").css("color","red");
+$("#feedback").html("No,but Google Drive is where you might share files and group documents");
 }
 			
 });
