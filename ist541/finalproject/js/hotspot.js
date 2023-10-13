32b363f6a690585fa4a405c423aef33571862c11
mist-denise-koch <123275394+mist-denise-koch@users.noreply.github.com>
2023-10-12 18:28:05 -0700

    edit image map

---
+++ ist541/finalproject/js/hotspot.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+
+$( function() {
+
+$("map area").click( function(){
+			
+$('#myModal').modal("show");
+
+if ($(this).attr("id") == "Gmail") {
+$("#feedback").css("color","red");
+$("#feedback").html("Although you can access Google Chat and Spaces, there is a faster way to meet in real time");
+}
+			
+if ($(this).attr("id") == "Meet") {
+$("#feedback").css("color","green");
+$("#feedback").html("Yes, accessing Google Meet allows you to meet virtually in real time to collaborate");
+}
+			
+if ($(this).attr("id") == "Drive") {
+$("#feedback").css("color","red");
+$("#feedback").html("No,but this is a great place to schedule a virtual meeting");
+}
+			
+if ($(this).attr("id") == "SharedDocs") {
+$("#feedback").css("color","red");
+$("#feedback").html("No,but this is a great place to collaborate on group documents");
+}
+			
+});
+	
+}); //end main jQuery function
+
+
+
+
